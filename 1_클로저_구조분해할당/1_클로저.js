df47bc5717ba9797cff35ec2d743c0c0020876c1
unan25 <kunan25@naver.com>
2023-11-20 10:12:02 +0900

    closure 예제

--- 1_클로저_구조분해할당/1_클로저.js
+++ 1_클로저_구조분해할당/1_클로저.js
@@ -3,5 +3,27 @@ function calculator(n1 ,n2) {
     function add() {
         return n1 + n2;
     }
+    // 함수 자체를 리턴하는 케이스, 11이 나올 것 같지만, closure 변수는 add 함수만 불러온다.
     return add;
+    
+    // 함수를 호출한 결과를 리턴하는 케이스
+    // return add();
 }
+
+// add() 11 response
+// const closure = calculator(5, 6);
+// console.log(closure); // -> [Function: add]   |   11 
+// console.log(typeof(closure)); // -> function  |   number
+
+function getVisitor() {
+    let visitCount = 0;
+    function coutnVisitor() {
+        return ++visitCount;
+    }
+    return coutnVisitor;
+}
+
+const countVisitor = getVisitor();
+console.log(countVisitor());
+console.log(countVisitor());
+console.log(countVisitor());
